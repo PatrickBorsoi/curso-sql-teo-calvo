dd9acc410c3eb061355da09db50d8efc74646997
Patrick Braga Borsoi Perez <Patrick.Perez@SEIOP.local>
2026-06-02 18:14:34 -0300

    feat: criando novas colunas, criando uma coluna de data

--- aulas/aula_01/05_select_col.sql
+++ aulas/aula_01/05_select_col.sql
@@ -3,5 +3,6 @@ SELECT idCliente,
         -- qtdePontos + 10 AS QtdePontosPlus10,
         -- qtdePontos * 2 AS QtdePontosDouble
         DtCriacao,
-        date(substr(DtCriacao, 1, 19)) AS Date
+        datetime(substr(DtCriacao, 1, 19)) AS dtCriacaoNova
+        strftime('%w', datetime(substr(DtCriacao, 1, 19))) AS diaSemana
 FROM clientes
