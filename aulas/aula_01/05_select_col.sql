d0c4433314750cb88a6a172f382d68aa67b83127
Patrick Perez <patrickperez1698@gmail.com>
2026-06-03 00:30:43 -0300

    fix: adicionando uma , que eu esqueci de adicionar

--- aulas/aula_01/05_select_col.sql
+++ aulas/aula_01/05_select_col.sql
@@ -1,8 +1,8 @@
 SELECT idCliente,
-        -- qtdePontos,
-        -- qtdePontos + 10 AS QtdePontosPlus10,
-        -- qtdePontos * 2 AS QtdePontosDouble
+        qtdePontos,
+        qtdePontos + 10 AS QtdePontosPlus10,
+        qtdePontos * 2 AS QtdePontosDouble,
         DtCriacao,
-        datetime(substr(DtCriacao, 1, 19)) AS dtCriacaoNova
+        datetime(substr(DtCriacao, 1, 19)) AS dtCriacaoNova,
         strftime('%w', datetime(substr(DtCriacao, 1, 19))) AS diaSemana
 FROM clientes
