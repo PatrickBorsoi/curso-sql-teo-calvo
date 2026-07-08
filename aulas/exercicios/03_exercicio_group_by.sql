3c1b9fe18e71195e7a11f7c22e4a82305f1b2884
Patrick Perez <patrickperez1698@gmail.com>
2026-07-07 23:24:16 -0300

    feat: exercicios com group_by

---
+++ aulas/exercicios/03_exercicio_group_by.sql
@@ -0,0 +1,31 @@
+-- Quantos clientes tem email cadastrado?
+
+-- SELECT sum(flEmail)
+-- FROM clientes
+
+-- Qual cliente juntou mais pontos positivos em 2025-05?
+
+-- SELECT idCliente, 
+--         sum(qtdePontos) AS totalPontos
+            , DtCriacao
+
+-- FROM transacoes
+
+-- WHERE qtdePontos > 0
+-- AND DtCriacao >= '2025-05-01'
+-- AND DtCriacao < '2025-06-01'
+
+-- GROUP BY idCliente
+
+-- ORDER BY  sum(qtdePontos) DESC
+
+-- LIMIT 1
+
+-- Qual cliente fez mais transações no ano de 2024?
+-- Quantos produtos são de rpg?
+-- Qual o valor médio de pontos positivos por dia?
+-- Qual dia da semana quem mais pedidos em 2025?
+-- Qual o produto mais transacionado?
+-- Qual o produto com mais pontos transacionados?
+
+
